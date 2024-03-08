451dcdc139b66c38dc8792b0c2c78752d2f3ee3d
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2024-03-08 10:33:40 -0300

    Refactoring tests

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -8,11 +8,17 @@ describe("GET /test", () => {
   });
 
   it("should return a list of users", async () => {
-    const response = await request(server).get("/test");
-    expect(response.body).toEqual([
+    // GIVEN
+    const users = [
       { id: 1, name: "Alice" },
       { id: 2, name: "Bob" },
       { id: 3, name: "Charlie" },
-    ]);
+    ];
+
+    // WHEN
+    const response = await request(server).get("/test");
+
+    // THEN
+    expect(response.body).toEqual(users);
   });
 });
